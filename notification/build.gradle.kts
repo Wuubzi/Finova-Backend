4d2ce3c74d54e7a555c872ba9a36dcef3cb79ce0
wuubzi <carlosasalas321@gmail.com>
2026-02-26 15:42:24 -0500

    fixing notification microsrvice of ci

--- notification/build.gradle.kts
+++ notification/build.gradle.kts
@@ -26,7 +26,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.springframework.cloud:spring-cloud-starter-config")
 	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
-	testImplementation("org.springframework.boot:spring-boot-starter-mail-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
--- notification/src/test/kotlin/com/wuubzi/notification/NotificationApplicationTests.kt
+++ notification/src/test/kotlin/com/wuubzi/notification/NotificationApplicationTests.kt
@@ -3,7 +3,14 @@ package com.wuubzi.notification
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
+@SpringBootTest(
+	properties = [
+		"spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.mail.MailSenderAutoConfiguration",
+		"spring.cloud.config.enabled=false",
+		"spring.config.import=optional:",
+		"eureka.client.enabled=false"
+	]
+)
 class NotificationApplicationTests {
 
 	@Test
